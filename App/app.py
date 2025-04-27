57e6d8bb0f0bdbb87ff54957858445f1c2c3a35e
ASHOK KUMAR <85332573+ashokumar06@users.noreply.github.com>
2025-04-27 12:36:10 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -86,7 +86,7 @@ st.markdown(
     }
     </style>
     <div class="button-container">
-        <a href="https://ashok-prajapati2.github.io/Portfolio/" target="_blank">About Me</a>
+        <a href="https://ashokumar.in" target="_blank">About Me</a>
     </div>
     """,
     unsafe_allow_html=True,
